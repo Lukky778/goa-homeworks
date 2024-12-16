a157d42a7335f2f3190b39c7a2ad59192ae6dca7
Lukky778 <goatunagoat@gmail.com>
2024-12-16 19:44:38 +0400

    second commit

---
+++ day 28/homework/homework.py
@@ -0,0 +1,35 @@
+# 1)
+def bool_to_word(boolean):
+    if boolean:
+        return "Yes"
+    else:
+        return "No"
+
+
+# 2)
+def remove_char(s):
+    return s[1:-1]
+
+
+# 3)
+def string_to_number(s):
+    return str(3)
+
+
+# 5)
+def sum_array(a):
+    counter = 0
+
+
+for num in a:
+    counter = counter + num
+counter += num
+"return" = counter
+
+
+# 4)
+def sum_array(a):
+    counter = 0
+    for i in range(len(a)):
+        counter += a[i]
+    return counter
