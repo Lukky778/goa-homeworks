00a3c7c27afcc17cbadc56a0fe7df23f800bb176
Lukky778 <goatunagoat@gmail.com>
2025-02-09 18:42:42 +0400

    second commit

--- day 44/classwork/cw.py
+++ day 44/classwork/cw.py
@@ -39,3 +39,31 @@ def twice_as_old(dad_years_old, son_years_old):
         return dad_years_old - 2 * son_years_old
     else:
         return 2 * son_years_old - dad_years_old
+
+#5
+def greet(language):
+    arr = [
+        ("english", "Welcome"),
+        ("czech", "Vitejte"),
+        ("danish", "Velkomst"),
+        ("dutch", "Welkom"),
+        ("estonian", "Tere tulemast"),
+        ("finnish", "Tervetuloa"),
+        ("flemish", "Welgekomen"),
+        ("french", "Bienvenue"),
+        ("german", "Willkommen"),
+        ("irish", "Failte"),
+        ("italian", "Benvenuto"),
+        ("latvian", "Gaidits"),
+        ("lithuanian", "Laukiamas"),
+        ("polish", "Witamy"),
+        ("spanish", "Bienvenido"),
+        ("swedish", "Valkommen"),
+        ("welsh", "Croeso")
+    ]
+    
+    for i in range(len(arr)):
+            if arr[i][0] == language:
+                return arr[i][1]
+    return "Welcome"
+    
